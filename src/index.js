9f949a20ee80e05d88e3ef07b1f30a6ffbf9a023
GiveMe-A-Name <a455300764b@hotmail.com>
2023-03-07 16:21:00 +0800

    fix: update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Main from "./main";
+import Main from "./Main";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(React.createElement(Main));
